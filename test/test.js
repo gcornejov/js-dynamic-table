2d5356b03e9a85f7c98dc0e3ade5d6f1e63a0623
Gustavo Cornejo <g.cornejovivado@gmail.com>
2021-11-14 17:00:00 -0300

    Add test cases for addRow() function

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
 // functions for testing
+document.getElementById('add-row').onclick = function () { 
+	//alert('Hello, this is my test alert.');
+
+	//addRow('dynamic-table-test', ['this', 'is', 'a', 'test', 'column']);
+
+	/*addRow('dynamic-table-test', ['this', 'is', 'a', 'test']);
+	addRow('dynamic-table-test', ['this', 'is', 'a', 'test', '2']);
+	addRow('dynamic-table-test', ['this', 'is', 'a', 'test', 'column', '3']);*/
+
+	addRow('dynamic-table-test', []);
+};
